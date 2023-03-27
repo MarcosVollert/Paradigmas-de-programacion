63bf94c1f9593c90783ee0f5b53fd66d0a2b5d17
MarcosVollert <107496482+MarcosVollert@users.noreply.github.com>
2023-03-26 23:50:18 -0300

    Update Reproductor.hs

--- Reproductor.hs
+++ Reproductor.hs
@@ -12,35 +12,46 @@ nuevoR file = RP file (nuevaP [])
 
 
 archivosR :: Reproductor ->FileSystem
-archivosR (RP file _) = file
+archivosR (RP file ) = file
 
 
 listaParaR :: Etiqueta -> Reproductor -> [Tema]
-listaParaR eti (RP file _) = [tema | tema <- temasF file, aplicaT eti tema]
+listaParaR eti (RP file ) = [tema | tema <- temasF file, aplicaT eti tema]
 
 
 temasR :: Reproductor ->[Tema]
-temasR (RP fs _) = temasF fs
+temasR (RP file ) = temasF file
 
 
 playR :: Reproductor -> Etiqueta -> Reproductor
-playR (RP fs play) eti = RP fs (nuevaP temas)
-  where temas = [tema | tema <- temasF fs, elem eti (etiquetasT tema)]
+playR (RP file play) eti = RP file (nuevaP temas)
+  where temas = [tema | tema <- temasF file, elem eti (etiquetasT tema)]
 
 
 
 actualR :: Reproductor -> Tema
-actualR (RP _ play) = actualP play
+actualR (RP  play) = actualP play
 
 
 avanzarR :: Reproductor -> Reproductor
-avanzarR (RP fs playlist) = RP fs (skipP playlist)
+avanzarR (RP file playlist) = RP file (skipP playlist)
 
 
 retrocederR :: Reproductor -> Reproductor
-retrocederR (RP fs play) = RP fs (backP play)
+retrocederR (RP file play) = RP file (backP play)
 
 
 reiniciarR :: Reproductor -> Reproductor
-reiniciarR (RP fs play) = RP fs (resetP play)
+reiniciarR (RP file play) = RP file (resetP play)
 
+test = [
+  nuevoR (nuevoF) == (RP (nuevoF) (nuevaP [])),
+  archivosR (RP (nuevoF) (nuevaP [])) == nuevoF,
+  listaParaR "etiquera" (RP (nuevoF) (nuevaP [])) == [],
+  temasR (RP (nuevoF) (nuevaP [])) == [],
+  playR  (RP (nuevoF) (nuevaP [])) "etiquera" == RP nuevoF (nuevaP []),
+  actualR (RP (nuevoF) (nuevaP [nuevoT "tema1" "a"])) == (nuevoT "tema1" "a"),
+  avanzarR (RP (nuevoF) (nuevaP [nuevoT "tema1" "a"])) == RP (nuevoF) (skipP((nuevaP [nuevoT "tema1" "a"]))),
+  retrocederR (RP (nuevoF) (nuevaP [nuevoT "tema1" "a"])) == RP (nuevoF) (backP((nuevaP [nuevoT "tema1" "a"]))),
+  reiniciarR (RP (nuevoF) (nuevaP [nuevoT "tema1" "a"])) == RP (nuevoF) (resetP((nuevaP [nuevoT "tema1" "a"])))
+  ]
